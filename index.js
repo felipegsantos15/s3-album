7c08a20090258eab68023a42c500a63e252da583
felipe.santos <felipe.santos@pontaltelecom.com.br>
2020-04-10 19:29:23 -0300

    atualizando funcção da index

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const restify = require('restify');
-const server = restify.createServer();
-const router = require('./router');
-
-router.router(server);
-
-server.listen(process.env.PORT, () => {
-    console.log(`server listen on port ${process.env.PORT}`);
-});
+require('dotenv').config()
+require(__dirname + '/app/');
