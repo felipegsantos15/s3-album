62c409ecaf8c567f4a4321eb8f37be5b669a22eb
felipe.santos <felipe.santos@pontaltelecom.com.br>
2020-04-14 22:19:54 -0300

    adcionado metodo para pegar informações da s3

---
+++ app/aws/s3-methods.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+    accessKeyId: process.env.KEY, secretAccessKey: process.env.SECRET, region: process.env.REGION
+});
+const s3 = new AWS.S3();
+
+module.exports = {
+    list: async () => {
+        try {
+            let objects = [];
+            const response = s3.listObjectsV2({
+                Bucket: "pointer-media",
+                MaxKeys: 10,
+                Prefix: "rbrasil"
+            }).promise();
+    
+            await response.then(data => {
+                for (let i = 0; i < data.Contents.length; i++) {
+                    objects.push("https://pointer-media.s3-sa-east-1.amazonaws.com/"+data.Contents[i].Key); 
+                }
+            });
+            return objects;
+        } catch (error) {
+            throw error;
+        }
+    }
+};
